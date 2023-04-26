9f8addb3a8391df457d0a38ad6c582b6d0483a1b
niksen111 <nikiksen2003@gmail.com>
2023-04-26 22:31:16 +0300

    feat: updated diagram

--- discount-cards/discount_cards_set_up.sql
+++ discount-cards/discount_cards_set_up.sql
@@ -6,7 +6,7 @@ USE discount_cards;
 
 CREATE TABLE Product_Type(
     Product_Type_ID        INTEGER         NOT NULL,
-    Category	           VARCHAR(50)     NOT NULL ,
+    Category	           VARCHAR(30)     NOT NULL ,
     Coef_Discount          FLOAT           NOT NULL,
     CONSTRAINT Product_Type_PK PRIMARY KEY (Product_Type_ID)
 );
